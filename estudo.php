23dda11839059a7690bbece720196473c70216c3
Lucca Rendall <luccarendall2@gmail.com>
2023-08-25 21:58:14 -0300

    Como criar variáveis e atribuir valores

---
+++ estudo.php
@@ -0,0 +1,12 @@
+
+<!--< ?php  o código php deve sempre vir após essa tag de inicio
+?> e antes dessa tag de final (desnecessária se o arquivo for apenas php)-->
+
+<!-- Como criar variável e atribuir valor-->
+<?php
+ $nomeVariavel = "valor"
+ $_nomeCurso = "Programação de Páginas Dinâmicas com PHP";
+ $ano_criacao = 1994;
+ $flagLinguagemScript = true;
+?>
+
